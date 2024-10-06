b742c328ae9dffc2204d44e622eb0c7ade9909c7
TheSebitzu <sebibonea@gmail.com>
2024-10-06 16:03:48 +0300

    0169-MAJORITY-ELEMENT Stats: Time: 172 ms (51.8%), Space: 17.9 MB (83.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,19 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Sort in ascending order
-        nums.sort()
-        i = 0
-        end = len(nums) - 1
-        n = len(nums)
-        length = 1
-        maj_elem = nums[0]
-        while i < end:
-            if nums[i] == nums[i + 1]:
-                length += 1
-                if length > int(n/2):
-                    maj_elem = nums[i]
-                    break
+        count = 0
+        current = None
+
+        for element in nums:
+            if count == 0:
+                current = element
+            if element == current:
+                count += 1
             else:
-                length = 1
-            i += 1
-        return maj_elem
+                count -= 1
+
+        return current
