625a2dc7a9e73fa7e392370c6c131a3ceefe876c
Sebastian Bonea <sebibonea@gmail.com>
2024-11-29 14:57:48 +0200

    0042-TRAPPING-RAIN-WATER Stats: Time: 32 ms (20.19%), Space: 18.9 MB (6.32%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,26 +1,27 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        height_len = len(height)
         # Left[i] = tallest bar to the left of i
-        left = [0] * len(height)
+        left = [0] * height_len
 
         # Right[i] = tallest bar to the right of i
-        right = [0] * len(height)
+        right = [0] * height_len
 
         # Find 
 
         # Left array
         left[0] = height[0]
-        for i in range(1, len(height)):
+        for i in range(1, height_len):
             left[i] = max(left[i - 1], height[i])
 
         # Right array
-        right[len(height) - 1] = height[len(height) - 1]
-        for i in range(len(height) - 2, -1, -1):
+        right[height_len - 1] = height[height_len - 1]
+        for i in range(height_len - 2, -1, -1):
             right[i] = max(right[i + 1], height[i])
 
         # Result
         res = 0
-        for i in range(0, len(height)):
+        for i in range(0, height_len):
             res += min(left[i], right[i]) - height[i]
 
         return(res)
