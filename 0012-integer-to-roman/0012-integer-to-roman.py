561e575fd07d4a408f1248535b7a491bee8db243
Sebastian Bonea <sebibonea@gmail.com>
2024-12-01 03:03:42 +0200

    0012-INTEGER-TO-ROMAN Stats: Time: 5 ms (54.19%), Space: 17.2 MB (5.66%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,26 +1,26 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         numbers = {
-            1: "I",
-            4: "IV",
-            5: "V",
-            9: "IX",
-            10: "X",
-            40: "XL",
-            50: "L",
-            90: "XC",
-            100: "C",
-            400: "CD",
-            500: "D",
-            900: "CM",
             1000: "M",
+            900: "CM",
+            500: "D",
+            400: "CD",
+            100: "C",
+            90: "XC",
+            50: "L",
+            40: "XL",
+            10: "X",
+            9: "IX",
+            5: "V",
+            4: "IV",
+            1: "I",
         }
 
         # Empty string for result
         result = ""
 
-        # From big to low
-        for key in reversed(numbers.keys()):
+        # From big to low (wrote the dict like this)
+        for key in numbers.keys():
 
             # If num can still be written as roman numerals
             while num >= key:
