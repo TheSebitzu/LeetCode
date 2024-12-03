8ad4230299f53dcf048243dfe5dd97172c83c64b
Sebastian Bonea <sebibonea@gmail.com>
2024-12-04 01:03:25 +0200

    0006-ZIGZAG-CONVERSION Stats: Time: 11 ms (63.78%), Space: 17.1 MB (20.15%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,25 +1,26 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        rows = [""] * numRows
+        # If one row, there is no zigzag
         if numRows == 1:
             return s
-        strs = [s[i:i + (numRows - 1) * 2] for i in range(0, len(s), (numRows - 1) * 2)]
+        
+        # We go down so need to add 1 to row_count
+        direction = 1 # down
+        row_count = 0
 
-        for s in strs:
-            if len(s) != (numRows - 1) * 2:
-                s += " " * ((numRows - 1) * 2 - len(s))
-            rows[0] += s[0]
-            first = 1
-            last = len(s) - 1
-            row = 1
-            while row != numRows and first <= last:
-                if first == last:
-                    rows[row] += s[first]
-                else:
-                    rows[row] += s[first] + s[last]
-                rows[row] = rows[row].strip()
-                first += 1
-                last -= 1
-                row += 1
+        # Make numRows empty strings
+        rows = [""] * numRows
 
+        for c in s:
+            # Put every character in right row
+            rows[row_count] += c
+
+            # Increment or decrement, depending on direction
+            row_count += direction
+
+            # If all the way down or all the way up, change direction
+            if row_count == 0 or row_count == numRows - 1:
+                direction *= -1
+
+        # Join rows to get output
         return "".join(rows)
